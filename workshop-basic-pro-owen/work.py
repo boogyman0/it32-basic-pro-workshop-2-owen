52e7f39630245589626b0dc365caa854a5e672d1
Student <Student@Train11-5-PC126.control.ad.sit.kmutt.ac.th>
2026-07-14 16:32:37 +0700

    เกือยเสร็จ

--- workshop-basic-pro-owen/work.py
+++ workshop-basic-pro-owen/work.py
@@ -4,5 +4,17 @@ sell = float(input("ราคาขาย : "))
 team_members = int(input("จำนวนลูกน้อง :"))
 cost_price = quantity * cost
 sell_price = quantity * sell
-gumrai = sell_price - cost_price
-
+total = sell_price - cost_price
+gumrai = total - sell_price
+boss_recive = total * 20/100
+member_recive = (total - boss_recive)/team_members
+print(f"จำนวนปืน {quantity}")
+print(f"ต้นทุนที่รับมา {cost}")
+print(f"ราคาขาย{sell}")
+print(f"จำนวนลูกน้อง {team_members}")
+print(f"{cost_price}")
+print(f"{sell_price}")
+print(f"{total}")
+print(f"{gumrai}")
+print(f"{boss_recive}")
+print(f"{member_recive}")
