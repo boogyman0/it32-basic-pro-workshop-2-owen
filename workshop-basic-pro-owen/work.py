99b6fbcb355e7fc6fa16808c370d308e0b9db892
boogyman0 <170705033+boogyman0@users.noreply.github.com>
2026-07-15 00:52:59 +0700

    เสร็จแล้วค้าบ

--- workshop-basic-pro-owen/work.py
+++ workshop-basic-pro-owen/work.py
@@ -5,16 +5,14 @@ team_members = int(input("จำนวนลูกน้อง :"))
 cost_price = quantity * cost
 sell_price = quantity * sell
 total = sell_price - cost_price
-gumrai = total - sell_price
 boss_recive = total * 20/100
 member_recive = (total - boss_recive)/team_members
 print(f"จำนวนปืน {quantity}")
 print(f"ต้นทุนที่รับมา {cost}")
 print(f"ราคาขาย{sell}")
 print(f"จำนวนลูกน้อง {team_members}")
-print(f"{cost_price}")
-print(f"{sell_price}")
-print(f"{total}")
-print(f"{gumrai}")
-print(f"{boss_recive}")
-print(f"{member_recive}")
+print(f"ต้นทุนราคา {cost_price}")
+print(f"มีรายได้รวม {sell_price}")
+print(f"เงินของบอส {boss_recive}")
+print(f"ลูกน้องจะได้คนล่ะ{member_recive}")
+
