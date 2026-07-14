72f59ca2da7c655a6665ac1005f40c4bf9359065
Student <Student@Train11-5-PC126.control.ad.sit.kmutt.ac.th>
2026-07-14 16:16:22 +0700

    ยังไม่เสร็จครับ

---
+++ workshop-basic-pro-owen/work.py
@@ -0,0 +1,8 @@
+quantity = int(input("จำนวนปืน :"))
+cost = float(input("ต้นทุนปืนที่รับมา :"))
+sell = float(input("ราคาขาย : "))
+team_members = int(input("จำนวนลูกน้อง :"))
+cost_price = quantity * cost
+sell_price = quantity * sell
+gumrai = sell_price - cost_price
+
